6bdba37033acb3afa4ed68df22e683a611b5c3fa
shahstudios <sshahcode@gmail.com>
2021-05-29 16:35:31 -0400

    objective 3

---
+++ app1/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Footer from './components/Footer'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+
+
+
+function App() {
+    return (
+        <>
+            <Header />
+            <MainContent />
+            <Footer />
+        </>
+    )
+}
+
+export default App
---
+++ app1/src/components/MainContent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MainContent() {
+    return (
+        <>
+        <h2>Main Content</h2>
+        </>
+    )
+}
+
+export default MainContent
