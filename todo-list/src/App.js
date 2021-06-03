9a2c2444f31fc0216ca983e05ee3f05d424cc28c
shahstudios <sshahcode@gmail.com>
2021-06-03 19:19:21 -0400

    convert function based components to class based in todo list

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,18 +1,27 @@
-import React from "react"
+import React, { Component } from "react"
 
 import TodoItem from '../src/components/TodoItem'
 import todosData from '../src/todosData'
 
 import '../src/styles.css'
 
-function App() {
-    const todoItems = todosData.map(item => <TodoItem key={item.id} item={item} />)
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+    }
 
-    return (
-        <div className="todo-list">
-            {todoItems}
-        </div>
-    )
+    render() {
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} />)
+
+        return (
+            <div className="todo-list">
+                {todoItems}
+            </div>
+        )
+    }
 }
 
 export default App
