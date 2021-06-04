8f6c41f0cc40cd0e7d6be0ee95fb55d10f84b7f7
shahstudios <sshahcode@gmail.com>
2021-06-04 16:02:51 -0400

    counter app, changing state

---
+++ counter-app/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+import '../src/styles.css'
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 0
+        }
+    }
+
+    handleClick() {
+
+    }
+
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
+}
+
+export default App
--- todo-list/src/components/TodoItem.js
+++ todo-list/src/components/TodoItem.js
@@ -4,7 +4,11 @@ class TodoItem extends Component {
     render() {
         return (
             <div className="todo-item">
-                <input type="checkbox" checked={this.props.item.completed}/>
+                <input 
+                    type="checkbox" 
+                    checked={this.props.item.completed} 
+                    onChange={() => console.log('debug')}
+                />
                 <p>{this.props.item.text}</p>
             </div>
         )
