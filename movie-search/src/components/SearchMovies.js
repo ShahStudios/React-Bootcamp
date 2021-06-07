d74eb13da83d4b072465ab4103147bea3e93cc47
shahstudios <sshahcode@gmail.com>
2021-06-06 20:30:32 -0400

    add useState

--- movie-search/src/components/SearchMovies.js
+++ movie-search/src/components/SearchMovies.js
@@ -1,21 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function SearchMovies() {
+    const [query, setQeury] = useState('')
+    const [movies, setMovies] = useState([])
 
     const searchMovies = async (e) => {
         e.preventDefault()
-        console.log('submitting')
-
-        const query = 'Jurassic Park'
 
         const url = `https://api.themoviedb.org/3/search/movie?api_key=b05db59ff9fa56a5ebc6d6dc860991f0&language=en-US&query=${query}&page=1&include_adult=false`;
 
         try {
-            const res = await fetch(url);
-            const data  = await res.json();
-            console.log(data);
+            const res = await fetch(url)
+            const data  = await res.json()
+            console.log(data.results)
+            setMovies(data.results)
         }catch(err){
-            console.error(err);
+            console.error(err)
         }
     }
 
@@ -23,7 +23,11 @@ export default function SearchMovies() {
     return (
         <form className='form' onSubmit={searchMovies}>
             <label className='label' htmlFor='query'>Movie Search</label>
-            <input className='input' type='text' name='query' placeholder='i.e. Marvel Movies'/>
+            <input className='input' type='text' name='query' placeholder='i.e. Marvel Movies'
+            value={query} onChange={(e) => {
+                setQeury(e.target.value)}
+            }
+            />
             <button className='button' type='submit'>Search</button>
         </form>
     )
