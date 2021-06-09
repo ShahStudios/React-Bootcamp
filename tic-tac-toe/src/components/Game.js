721f2c68aa7ae4c8199e930a0fbbadc82ea3a726
shahstudios <sshahcode@gmail.com>
2021-06-09 17:13:25 -0400

    finish up last logic for completing the game

--- tic-tac-toe/src/components/Game.js
+++ tic-tac-toe/src/components/Game.js
@@ -8,13 +8,39 @@ function Game() {
     const [xIsNext, setXIsNext] = useState(true)
     const winner = calculateWinner(board)
 
-    const handleClick = () => {}
-    const jumpTo = () => {}
-    const renderMoves = () => {}
+    const handleClick = i => {
+        const boardClone = [...board];
+
+        // If user click an square or if game is won, return
+        if (winner || boardClone[i]) return;
+
+        // Put an X or an O in the clicked square
+        boardClone[i] = xIsNext ? 'X' : 'O';
+        setBoard(boardClone);
+        setXIsNext(!xIsNext);
+    }
+
+    const jumpTo = () => {
+
+    }
+
+    const renderMoves = () => {
+        <button onClick={() => {
+            setBoard(Array(9).fill(null))
+        }}>
+            Start Game
+        </button>
+    }
 
     return (
         <>
+            <h1 className='recap'>Tic-Tac-Toe Game</h1>
             <Board squares={board} onClick={handleClick} />
+            <div className="recap">
+                <p>
+                    {winner ? 'Winner: ' + winner : 'Next Player: ' + (xIsNext ? 'X' : 'O')}
+                </p>
+            </div>
         </>
     )
 }
